a59fb13593a64d677c79ce70b8cacc5e2c8f1a2b
verklickt <verklickttwitch@gmx.de>
2023-10-03 19:55:11 +0200

    depends: Removed Problemo and updated Base to 0.0.8

--- backend-api/build.gradle.kts
+++ backend-api/build.gradle.kts
@@ -4,10 +4,7 @@ java {
 }
 
 val baseVersion: String by project.extra
-val problemoVersion: String by project.extra
 dependencies {
     api("com.uroria:base:${baseVersion}")
     annotationProcessor("com.uroria:base:${baseVersion}")
-
-    api("com.uroria:Problemo:${problemoVersion}")
 }
