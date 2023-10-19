a920464cbb3621bdb5e82164d7f5a1d28d187a92
verklickt <verklickttwitch@gmx.de>
2023-10-19 12:52:58 +0200

    depends: Updated Base to 0.0.9

--- backend-api/src/main/java/com/uroria/backend/user/User.java
+++ backend-api/src/main/java/com/uroria/backend/user/User.java
@@ -5,6 +5,7 @@ import com.uroria.backend.clan.Clan;
 import com.uroria.backend.user.crew.CrewHolder;
 import com.uroria.backend.user.punishment.Punishable;
 import com.uroria.base.lang.Language;
+import com.uroria.base.lang.Languageable;
 import com.uroria.base.user.UserStatus;
 import com.uroria.problemo.result.Result;
 import lombok.NonNull;
@@ -19,15 +20,13 @@ import java.util.UUID;
  * Everything you need from database is in here.
  * Note: Every method here may trigger a database query. This could cost some time.
  */
-public interface User extends Punishable, CrewHolder, Player {
+public interface User extends Punishable, CrewHolder, Player, Languageable {
 
     Optional<Long> getDiscordUserId();
 
     void setDiscordUserId(long id);
 
     @NotNull String getUsername();
-
-    @NotNull Language getLanguage();
 
     boolean isOnline();
 
